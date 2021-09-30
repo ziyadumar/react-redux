b62d43431349ddcadca061448ce89095983d7582
Ziyad Umar <ziyad96@gmail.com>
2021-09-30 17:19:50 +0200

    Add helper functions

---
+++ src/Helpers/functions.test.tsx
@@ -0,0 +1,6 @@
+import { mutliply } from "./functions"
+
+
+test('multiplytest', () => {
+    expect(mutliply(2, 3)).toBe(6);
+})
---
+++ src/Helpers/functions.tsx
@@ -0,0 +1,7 @@
+export const mutliply = (val1: number, val2: number) => {
+    return val1 * val2;
+}
+
+export const uppercase = (val: string) => {
+    return val.toUpperCase();
+}
